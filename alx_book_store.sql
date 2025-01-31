6406a6ff44929435660dea805c82ea57066270bc
George Monibah <monibahgeorgew@gmail.com>
2025-01-31 10:26:25 +0000

    alx_DB

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -28,7 +28,7 @@ CREATE TABLE Orders (
 	order_id INT AUTO_INCREMENT PRIMARY KEY,
 	customer_id INT,
 	order_date DATE NOT NULL,
-	FOREIGN KEY (customer_id) REFERENCES Customers (customer_id) ON DELETE CASCADE
+	FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details (
@@ -36,7 +36,7 @@ CREATE TABLE Order_Details (
 	order_id INT,
 	book_id INT,
 	quantity DOUBLE NOT NULL,
-	FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE,
-	FOREIGN KEY (book_id) REFERENCES Books(book_id) ON DELETE CASCADE
+	FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+	FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
 
