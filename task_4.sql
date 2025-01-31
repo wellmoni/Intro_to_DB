cc96be7ce06a9528ec1ca4b8215d368e2215b358
George Wellington Monibah <wellmoni95@gmail.com>
2025-01-31 11:41:48 +0000

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,3 @@
- TABLE_SCHEMA ='alx_book_store', TABLE NAME = 'Books';
+"INFORMATION_SCHEMA.COLUMNS", "SELECT", "COLUMN_NAME", "COLUMN_TYPE", "TABLE_SCHEMA = 'alx_book_store'", "TABLE_NAME = 'Books'TABLE_SCHEMA ='alx_book_store', TABLE NAME = 'Books';
 
 
